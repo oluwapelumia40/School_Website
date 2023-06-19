e23babba2eaf9574bed29b70dad847a10f2f3260
oluwapelumia40 <69686839+oluwapelumia40@users.noreply.github.com>
2023-06-19 18:47:28 +0100

    Student Login and Register Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import SLogin from './Auth/SLogin';
+import SRegister from './Auth/SRegister';
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<SLogin/>}/>
+          <Route path="/sregister" exact element={<SRegister/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
